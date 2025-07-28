9f6329a7b2fd3ea36cbde39d246097429b1c96db
IMYdev <omranmansour096@gmail.com>
2025-07-28 16:13:31 +0200

    Fix log_error call to await its execution in IG downloader.

--- src/serverless/modules/downloader.py
+++ src/serverless/modules/downloader.py
@@ -13,4 +13,4 @@ async def download_instagram_reel(m):
         modified_url = re.sub(r'(https?://)(www\.)?instagram', r'\1\2kkinstagram', insta_url)
         await bot.send_video(m.chat.id, modified_url)
     except Exception as e:
-        log_error(bot, e, m)
+        await log_error(bot, e, m)
